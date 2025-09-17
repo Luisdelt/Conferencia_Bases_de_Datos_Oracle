8d9fffec4efe99e589579e307ec65b0e378e8100
luisdelt <3637590850101@ingenieria.usac.edu.gt>
2025-09-17 12:06:33 -0600

    feat: consultas por métodos GET

--- Proyecto/backend/app.py
+++ Proyecto/backend/app.py
@@ -22,6 +22,185 @@ def dual():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route('/api/test/grades', methods=['GET'])
+def get_students_grades():
+    try:
+        connection = get_oracle_connection()
+        cursor = connection.cursor()
+        query = """
+        SELECT
+            r.id_registro,
+            r.nombre_completo,
+            r.genero,
+            r.tipo_licencia,
+            r.tipo_tramite,
+            e.id_examen,
+            rt.resultado_teorico,
+            rp.resultado_practico,
+            CASE
+                WHEN rt.resultado_teorico >= 70 AND rp.resultado_practico >= 70 THEN 'Aprobado'
+                ELSE 'Reprobado'
+            END AS resultado
+        FROM registro r
+        JOIN examen e ON r.id_registro = e.id_registro
+        JOIN (
+            SELECT ru.id_examen, SUM(CASE WHEN ru.respuesta = p.respuesta THEN 4 ELSE 0 END) AS resultado_teorico
+            FROM respuesta_usuario ru
+            JOIN pregunta p ON ru.id_pregunta = p.id_pregunta
+            GROUP BY ru.id_examen
+        ) rt ON e.id_examen = rt.id_examen
+        JOIN (
+            SELECT rpu.id_examen, SUM(rpu.nota) AS resultado_practico
+            FROM respuesta_practico_usuario rpu
+            GROUP BY rpu.id_examen
+        ) rp ON e.id_examen = rp.id_examen
+        WHERE TRUNC(r.fecha) = TO_DATE('13-03-23', 'DD-MM-YY')
+        ORDER BY e.id_examen"""
+        cursor.execute(query)
+        grades = [
+            {
+                "id_registro": row[0],
+                "nombre_completo": row[1],
+                "genero": row[2],
+                "tipo_licencia": row[3],
+                "tipo_tramite": row[4],
+                "id_examen": row[5],
+                "resultado_teorico": row[6],
+                "resultado_practico": row[7],
+                "resultado": row[8]
+            }
+            for row in cursor.fetchall()
+        ]
+        cursor.close()
+        connection.close()
+        return jsonify(grades)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+@app.route('/api/test/aproved', methods=['GET'])
+def get_aproved_grades():
+    try:
+        connection = get_oracle_connection()
+        cursor = connection.cursor()
+        query = """
+        SELECT
+            r.fecha,
+            r.id_registro,
+            r.nombre_completo,
+            r.genero,
+            r.tipo_licencia,
+            r.tipo_tramite,
+            e.id_examen,
+            rt.resultado_teorico,
+            rp.resultado_practico,
+            CASE
+                WHEN rt.resultado_teorico >= 70 AND rp.resultado_practico >= 70 THEN 'Aprobado'
+                ELSE 'Reprobado'
+            END AS resultado
+        FROM registro r
+        JOIN examen e ON r.id_registro = e.id_registro
+        JOIN (
+            SELECT ru.id_examen, SUM(CASE WHEN ru.respuesta = p.respuesta THEN 4 ELSE 0 END) AS resultado_teorico
+            FROM respuesta_usuario ru
+            JOIN pregunta p ON ru.id_pregunta = p.id_pregunta
+            GROUP BY ru.id_examen
+        ) rt ON e.id_examen = rt.id_examen
+        JOIN (
+            SELECT rpu.id_examen, SUM(rpu.nota) AS resultado_practico
+            FROM respuesta_practico_usuario rpu
+            GROUP BY rpu.id_examen
+        ) rp ON e.id_examen = rp.id_examen
+        WHERE TRUNC(r.fecha) BETWEEN TO_DATE('12-03-23', 'DD-MM-YY')
+        AND TO_DATE('13-03-23', 'DD-MM-YY')
+        AND rt.resultado_teorico >= 70
+        AND rp.resultado_practico >= 70
+        ORDER BY e.id_examen"""
+        cursor.execute(query)
+        aproved = [
+            {
+                "fecha": row[0],
+                "id_registro": row[1],
+                "nombre_completo": row[2],
+                "genero": row[3],
+                "tipo_licencia": row[4],
+                "tipo_tramite": row[5],
+                "id_examen": row[6],
+                "resultado_teorico": row[7],
+                "resultado_practico": row[8],
+                "resultado": row[9]
+            }
+            for row in cursor.fetchall()
+        ]
+        cursor.close()
+        connection.close()
+        return jsonify(aproved)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/test/count', methods=['GET'])
+def get_count_evaluations():
+    try:
+        connection = get_oracle_connection()
+        cursor = connection.cursor()
+        query = """
+        SELECT
+            r.tipo_tramite,
+            r.tipo_licencia,
+            COUNT(*) AS numero_evaluaciones
+        FROM registro r
+        WHERE TRUNC(r.fecha) BETWEEN TO_DATE('01-03-23', 'DD-MM-YY')  AND TO_DATE('13-03-23', 'DD-MM-YY')
+        GROUP BY r.tipo_tramite, r.tipo_licencia
+        ORDER BY r.tipo_tramite, r.tipo_licencia"""
+        cursor.execute(query)
+        counts = [
+            {
+                "tipo_tramite": row[0],
+                "tipo_licencia": row[1],
+                "numero_evaluaciones": row[2]
+            }
+            for row in cursor.fetchall()
+        ]
+        cursor.close()
+        connection.close()
+        return jsonify(counts)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/test/questions', methods=['GET'])
+def get_most_correct_questions():
+    try:
+        connection = get_oracle_connection()
+        cursor = connection.cursor()
+        query = """
+        SELECT
+            p.id_pregunta,
+            p.texto,
+            SUM(CASE WHEN ru.respuesta = p.respuesta THEN 1 ELSE 0 END) AS aciertos
+        FROM
+            registro r
+        JOIN examen e ON r.id_registro = e.id_registro
+        JOIN respuesta_usuario ru ON e.id_examen = ru.id_examen
+        JOIN pregunta p ON ru.id_pregunta = p.id_pregunta
+        WHERE TRUNC(r.fecha) BETWEEN TO_DATE ('12-03-23', 'DD-MM-YY')
+            AND TO_DATE('13-03-23', 'DD-MM-YY')
+        GROUP BY p.id_pregunta, p.texto
+        ORDER BY aciertos DESC"""
+        cursor.execute(query)
+        questions = [
+            {
+                "id_pregunta": row[0],
+                "texto": row[1],
+                "aciertos": row[2]
+            }
+            for row in cursor.fetchall()
+        ]
+        cursor.close()
+        connection.close()
+        return jsonify(questions)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(threaded=True, port=3000, debug=True)
